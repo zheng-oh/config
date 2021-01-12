78cc0cdb1c319b575d38e729fb94a08d3d9d7955
zheng-oh <894389673@qq.com>
2021-01-12 22:47:00 +0800

    homeconfig google-ext scripts

---
+++ homeconfig/scripts/volume.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+l_volume=`amixer get Master | awk '/Limits/{ print $NF }'`
+c_volume=`amixer get Master | awk -F'[ []' 'END { print $6 }'`
+
+rate=10 #音量变化的百分比
+
+function ajvolume(){
+	echo $c_volume
+	if [ $c_volume -lt 0 ]; then
+		let c_volume=0
+	elif [ $c_volume -gt $l_volume ];then
+		let c_volume=$l_volume
+	fi
+	amixer sset Master $c_volume
+	xsetroot -name "IP:$(ip addr show ens33| awk '/ens33/{print $2}'|tail -1)|Volume:$(amixer get Master | awk -F'[][]' 'END { print $2 }')|$(LC_ALL='C' date +'%F[%b %a] %R')"
+}
+if [ $1 = "+" ];then
+	if [ $c_volume != $l_volume ]; then
+		let c_volume+=$[l_volume*$rate/100]
+		ajvolume
+	fi
+elif [ $1 = "-" ];then
+	if [ $c_volume != 0 ]; then
+		let c_volume-=$[l_volume*$rate/100]
+		ajvolume
+	fi
+else
+	echo 参数错误
+fi
+
