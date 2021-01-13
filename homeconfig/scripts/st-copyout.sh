661087902c0a2526545d12bb7ff86013e82cfaee
zheng-oh <894389673@qq.com>
2021-01-13 16:21:19 +0800

    add copyout scripts

--- homeconfig/scripts/st-copyout.sh
+++ homeconfig/scripts/st-copyout.sh
@@ -1,12 +1,5 @@
-#!/bin/sh
-# Using external pipe with st, give a dmenu prompt of recent commands,
-# allowing the user to copy the output of one.
+#!/bin/fish
 # xclip required for this script.
-# By Jaywalker and Luke
-tmpfile=$(mktemp /tmp/st-cmd-output.XXXXXX)
-trap 'rm "$tmpfile"' 0 1 15
-sed -n "w $tmpfile"
-ps1="$(grep "\S" "$tmpfile" | tail -n 1 | sed 's/^\s*//' | cut -d' ' -f1)"
-chosen="$(grep -F "$ps1" "$tmpfile" | sed '$ d' | tac | dmenu -p "Copy which command's output?" -i -l 10 | sed 's/[^^]/[&]/g; s/\^/\\^/g')"
-eps1="$(echo "$ps1" | sed 's/[^^]/[&]/g; s/\^/\\^/g')"
-awk "/^$chosen$/{p=1;print;next} p&&/$eps1/{p=0};p" "$tmpfile" | xclip -selection clipboard
+# From scrapyer, https://github.com/zheng-oh
+set chose (history | head -n 5 | dmenu -i -p 'Copy which command out?' -l 5)
+eval $chose | xclip -sel clib 
