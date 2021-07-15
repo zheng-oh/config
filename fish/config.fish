9b6ef32a1fdc6eacb3da78dc98797175174ec45d
zheng-oh <894389673@qq.com>
2021-07-15 11:56:03 +0800

    add go path

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,5 @@
 set -x PATH $HOME/application/miniconda3/bin $PATH
+set -x PATH $HOME/go/bin $PATH
 set -g -x RANGER_LOAD_DEFAULT_RC FALSE
 set -g -x QT_AUTO_SCREEN_SCALE_FACTOR 0
 function fish_greeting
