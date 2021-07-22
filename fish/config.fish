4e383fac6080d219d267e1d9edb4afa0f09ebc62
邢政 <xingzheng@zxmac.local>
2021-07-22 22:11:21 +0800

    add go path

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,6 @@
 set -x PATH $HOME/miniconda3/bin $PATH
+set -x PATH $HOME/go/bin $PATH
+set -x PATH $HOME/.npm-global/bin $PATH
 set -g -x RANGER_LOAD_DEFAULT_RC FALSE
 set -g -x HOMEBREW_NO_AUTO_UPDATE TRUE
 function fish_greeting
